cac7e9b85ca686e42e1be6407b81f0c6ca641231
Sampjam <sampjam@gmail.com>
2022-05-02 13:57:40 -0400

    creating simple child component

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,32 +1,53 @@
-import { Component, NO_ERRORS_SCHEMA } from "@angular/core"
-import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { Component, Input, NO_ERRORS_SCHEMA, Output } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { of } from "rxjs";
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
-import { HeroesComponent } from "./heroes.component"
+import { HeroesComponent } from "./heroes.component";
 
-describe('HeroesComponent (shallowTests', () => {
+describe("HeroesComponent (shallowTests", () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES: Hero[];
-  beforeEach(() => {
-     mockHeroService = jasmine.createSpyObj(['getHeroes','addHero', 'deleteHero']);
-     HEROES = [ { id: 1, name: 'SuperDude', strength: 50}, {id: 2, name: 'WonderGal', strength: 70}]
-    TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
-      providers: [
-        {provide: HeroService, useValue: mockHeroService}
-      ],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-    fixture = TestBed.createComponent(HeroesComponent);
 
+  @Component({
+    selector: "app-hero",
+    template: "<div></div>",
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+    // @Output() delete = new EventEmitter();
+  }
+
+  beforeEach(() => {
+    mockHeroService = jasmine.createSpyObj([
+      "getHeroes",
+      "addHero",
+      "deleteHero",
+    ]);
+    HEROES = [
+      { id: 1, name: "SuperDude", strength: 50 },
+      { id: 2, name: "WonderGal", strength: 70 },
+    ];
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent, FakeHeroComponent],
+      providers: [{ provide: HeroService, useValue: mockHeroService }],
+      // schemas: [NO_ERRORS_SCHEMA] - we can comment this out if we use the FakeHeroComponent
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
   });
 
-  it('should set heroes correctly from the service', () => {
-    mockHeroService.getHeroes.and.returnValue(of(HEROES))
+  it("should set heroes correctly from the service", () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
     fixture.detectChanges();
     expect(fixture.componentInstance.heroes.length).toBe(2);
-  })
+  });
 
-})
+  it("should create 1 li for each hero", () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css("li")).length).toBe(2);
+  });
+});
